aac9ff8791f27433198ac20142c089afef620a02
Gardenia Georgia Barbosa De Siqueira <venyustech@gmail.com>
2022-03-07 08:48:08 -0300

    feat: getCustomersById

--- src/controllers/customersControllers.js
+++ src/controllers/customersControllers.js
@@ -25,9 +25,13 @@ export async function getCustomers(req, res) {
     }
 }
 export async function getCustomerById(req, res) {
-
+    const { id } = req.params;
     try {
-        res.send("ok");
+        const result = await db.query(`SELECT * FROM customers WHERE id = $1`, [id]);
+        if (result.rowCount === 0)
+            return res.status(404).send("id não existente");
+        res.send(result.rows[0]);
+
     } catch (error) {
         res.status(500).send(error);
     }
